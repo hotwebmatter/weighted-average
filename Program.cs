d32b4147a17de0eaba4d8e7b54c27a21856d137c
Matt Obert on Windows <matt@hotwebmatter.com>
2019-12-13 20:21:45 -0500

    Initial commit of weighted-average solution

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace weighted_average
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
